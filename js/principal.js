49c80bd24d12b61ba1b4b8040d724b0407a6ddaa
Francisco Javier Regaña Gil <giljamasev@gmail.com>
2015-12-23 12:00:30 +0100

    Actualización

--- js/principal.js
+++ js/principal.js
@@ -9,20 +9,6 @@ var oAcademia = new Academia();
 var altaAlu =document.querySelectorAll('.dropdown-menu')[0].children[0].children[0];
 altaAlu.addEventListener("click",mostrarAltaAlumno,false);
 
-var formAltAlu = document.forAltAlu.enviar;
-formAltAlu.addEventListener("click",altaAlumno,false);	
-
-
-
-/*-----------------------------------------------------------Fin Eventos-----------------------------------------------------------*/
-
-/*-----------------------------------------------------------Funciones gr�ficas-----------------------------------------------------------*/
-/*-----------------------------------------------------------Eventos-----------------------------------------------------------*/
-//TODO
-
-var altaAlu =document.querySelectorAll('.dropdown-menu')[0].children[0].children[0];
-altaAlu.addEventListener("click",mostrarAltaAlumno,false);
-
 var modiAlu =document.querySelectorAll('.dropdown-menu')[0].children[1].children[0];
 modiAlu.addEventListener("click",mostrarModi,false);
 
@@ -30,59 +16,43 @@ modiAlu.addEventListener("click",mostrarModi,false);
 var bajaAlu =document.querySelectorAll('.dropdown-menu')[0].children[3].children[0];
 bajaAlu.addEventListener("click",mostrarBaja,false);
 
+var formAltAlu = document.forAltAlu.enviar;
+formAltAlu.addEventListener("click",altaAlumno,false);	
 
 
-/*-----------------------------------------------------------Fin Funciones gr�ficas-----------------------------------------------------------*/
 
+/*-----------------------------------------------------------Fin Eventos-----------------------------------------------------------*/
 
-/*-----------------------------------------------------------Funciones gr�ficas-----------------------------------------------------------*/
+/*-----------------------------------------------------------Funciones gráficas-----------------------------------------------------------*/
 function mostrarAltaAlumno(oEvento) {
-
-	ocultarFormularios()
-
+	ocultarFormularios();
 	var oE = oEvento || window.event;     // Guarda el Objeto que ha disparado el evento
-	if (oE.target == oE.currentTarget) { //el objetivo del evento al que le has registrado el evento es el mismo
-
+	if (oE.target == oE.currentTarget)   //el objetivo del evento al que le has registrado el evento es el mismo
+	{
 		var oFormu = document.getElementById("forAltAlu");
 		oFormu.style.display = "block";
-
-
 	}
 }
 
 function mostrarModi(oEvento) {
-
-	ocultarFormularios()
-
+	ocultarFormularios();
 	var oE = oEvento || window.event;     // Guarda el Objeto que ha disparado el evento
-	if (oE.target == oE.currentTarget) { //el objetivo del evento al que le has registrado el evento es el mismo
-
+	if (oE.target == oE.currentTarget)  //el objetivo del evento al que le has registrado el evento es el mismo
+	{
 		var oFormu = document.getElementById("forAluMod");
 		oFormu.style.display = "block";
-
-
-
 	}
-
 }
 
 function mostrarBaja(oEvento) {
-
-	ocultarFormularios()
-
-	var oE = oEvento || window.event;     // Guarda el Objeto que ha disparado el eventoa
-	if (oE.target == oE.currentTarget) { //el objetivo del evento al que le has registrado el evento es el mismo
-
+	ocultarFormularios();
+	var oE = oEvento || window.event;     // Guarda el Objeto que ha disparado el evento
+	if (oE.target == oE.currentTarget)  //el objetivo del evento al que le has registrado el evento es el mismo
+	{
 		var oFormu = document.getElementById("forBajAlum");
 		oFormu.style.display = "block";
-
-
-
 	}
-
 }
-
-
 
 function ocultarAltaAlumno()
 {
@@ -90,15 +60,12 @@ function ocultarAltaAlumno()
 	oFormu.txtDni.value="";
 	oFormu.txtNombre.value="";
 	oFormu.txtApellidos.value="";
-	oFormu.display="none";
+	oFormu.style.display="none";
 }
 
-
-function ocultarFormularios(){
-
-	var oCapaAlta = document.getElementById("forAltAlu");
-	oCapaAlta.style.display="none";
-
+function ocultarFormularios()
+{
+	ocultarAltaAlumno();
 	var oCapaMod = document.getElementById("forAluMod");
 	oCapaMod.style.display="none";
 
@@ -107,11 +74,7 @@ function ocultarFormularios(){
 
 
 }
-
-
-/*-----------------------------------------------------------Fin Funciones gr�ficas-----------------------------------------------------------*/
-
-
+/*-----------------------------------------------------------Fin Funciones gráficas-----------------------------------------------------------*/
 
 /*-----------------------------------------------------------Alta Alumno-----------------------------------------------------------*/
 function altaAlumno(oEvento)
@@ -127,7 +90,7 @@ function altaAlumno(oEvento)
 	}
 	else
 	{
-		mensajeError();
+		mensajeError(oForm);
 	}
 }
 /*-----------------------------------------------------------Fin Alta Alumno-----------------------------------------------------------*/
@@ -135,11 +98,16 @@ function altaAlumno(oEvento)
 /*-----------------------------------------------------------Auxiliares-----------------------------------------------------------*/
 function validaciones(oForm)
 {
-	//TODO
-	return false;
+	var bValido = true;
+	for(var i=0;i<oForm.elements.length-1;i++)
+	{
+		if(oForm.elements[i].value.trim()=="")
+			bValido=false;
+	}
+	return bValido;
 }
 
-function mensajeError()
+function mensajeError(oForm)
 {
 	var sError="";
 	
