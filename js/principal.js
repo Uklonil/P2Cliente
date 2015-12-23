8d8e9ddf4233c27c62382844a88eef634fdddfbd
Antonio Jose <antoniojsanchez92@gmail.com>
2015-12-23 11:47:28 +0100

    retoques

--- js/principal.js
+++ js/principal.js
@@ -17,15 +17,72 @@ formAltAlu.addEventListener("click",altaAlumno,false);
 /*-----------------------------------------------------------Fin Eventos-----------------------------------------------------------*/
 
 /*-----------------------------------------------------------Funciones gr�ficas-----------------------------------------------------------*/
+/*-----------------------------------------------------------Eventos-----------------------------------------------------------*/
+//TODO
+
+var altaAlu =document.querySelectorAll('.dropdown-menu')[0].children[0].children[0];
+altaAlu.addEventListener("click",mostrarAltaAlumno,false);
+
+var modiAlu =document.querySelectorAll('.dropdown-menu')[0].children[1].children[0];
+modiAlu.addEventListener("click",mostrarModi,false);
+
+
+var bajaAlu =document.querySelectorAll('.dropdown-menu')[0].children[3].children[0];
+bajaAlu.addEventListener("click",mostrarBaja,false);
+
+
+
+/*-----------------------------------------------------------Fin Funciones gr�ficas-----------------------------------------------------------*/
+
+
+/*-----------------------------------------------------------Funciones gr�ficas-----------------------------------------------------------*/
 function mostrarAltaAlumno(oEvento) {
+
+	ocultarFormularios()
 
 	var oE = oEvento || window.event;     // Guarda el Objeto que ha disparado el evento
 	if (oE.target == oE.currentTarget) { //el objetivo del evento al que le has registrado el evento es el mismo
 
 		var oFormu = document.getElementById("forAltAlu");
 		oFormu.style.display = "block";
+
+
 	}
 }
+
+function mostrarModi(oEvento) {
+
+	ocultarFormularios()
+
+	var oE = oEvento || window.event;     // Guarda el Objeto que ha disparado el evento
+	if (oE.target == oE.currentTarget) { //el objetivo del evento al que le has registrado el evento es el mismo
+
+		var oFormu = document.getElementById("forAluMod");
+		oFormu.style.display = "block";
+
+
+
+	}
+
+}
+
+function mostrarBaja(oEvento) {
+
+	ocultarFormularios()
+
+	var oE = oEvento || window.event;     // Guarda el Objeto que ha disparado el evento
+	if (oE.target == oE.currentTarget) { //el objetivo del evento al que le has registrado el evento es el mismo
+
+		var oFormu = document.getElementById("forBajAlum");
+		oFormu.style.display = "block";
+
+
+
+	}
+
+}
+
+
 
 function ocultarAltaAlumno()
 {
@@ -35,7 +92,26 @@ function ocultarAltaAlumno()
 	oFormu.txtApellidos.value="";
 	oFormu.display="none";
 }
+
+
+function ocultarFormularios(){
+
+	var oCapaAlta = document.getElementById("forAltAlu");
+	oCapaAlta.style.display="none";
+
+	var oCapaMod = document.getElementById("forAluMod");
+	oCapaMod.style.display="none";
+
+	var oCapaBaj = document.getElementById("forBajAlum");
+	oCapaBaj.style.display="none";
+
+
+}
+
+
 /*-----------------------------------------------------------Fin Funciones gr�ficas-----------------------------------------------------------*/
+
+
 
 /*-----------------------------------------------------------Alta Alumno-----------------------------------------------------------*/
 function altaAlumno(oEvento)
